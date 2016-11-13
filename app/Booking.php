a60e9629b0c00053442452be6f9a9fe4b61b0714
mas <ma99.dev@gmail.com>
2016-11-13 12:45:54 +0300

    Add Route Search

--- app/Booking.php
+++ app/Booking.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     public $incrementing = false;
+
+    public function schedule()
+    {
+    	return $this->belongsTo('Schedule');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +18,27 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/search', function(){
+	   
+	$from  = 'dhaka';
+	$to = 'sylhet';
+	// $route = \App\Rout::where('departure_city', $from)->
+	// 				where( 'arrival_city', $to)->first();
+	//$routeId= $route->id;
+
+	//$email = DB::table('users')->where('name', 'John')->value('email');
+	$routeId= \App\Rout::where('departure_city', $from)->
+					where( 'arrival_city', $to)->value('id');
+	
+	//return($routeId);
+	// all bookings with a particular route id
+	//$schedules = \App\Schedule::where('rout_id', $routeId)->with(‘bookings’)->get();
+	$schedules = \App\Schedule::where('rout_id', $routeId)->get();
+	$sch = $schedules->load('bookings');
+
+	return($sch);
+
+
+
+});
